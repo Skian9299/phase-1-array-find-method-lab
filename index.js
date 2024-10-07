44b455cf37ad8feca46e2672f794e00e9dcab2fb
Ian Kimutai <kimutaiian2@gmail.com>
2024-10-07 08:50:39 +0300

    commit index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record) {
+    // Find the first win object in the record array
+    let win = record.find(game => game.result === 'W');
+  
+    // If a win is found, return the year, otherwise return undefined
+    return win ? win.year : undefined;
+  }
+  
